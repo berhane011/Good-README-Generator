e1fa438e61211cdefa1ec8f519fdd4974f0d388a
berhane <berhane4321@gmail.com>
2020-06-07 01:57:00 -0700

    generate a README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,6 @@
-# <h1 style =" text-align:center;"> dfsdafdsda</h1>
+// function to generate markdown for README
+function generateMarkdown(data) {
+  return `# <h1 style =" text-align:center;"> ${data.title}</h1>
 
 
 <!-- PROJECT LOGO -->
@@ -8,10 +10,10 @@
     <img src="images/logo.png" alt="Logo" width="80" height="80">
   </a>
 
-  <h style =" text-align:center;">dfsdafdsda</h>
+  <h style =" text-align:center;">${data.title}</h>
 
   <p style =" text-align:center;">
-    dsfasfdsa
+    ${data.description}
     <br />
     <a href="https://github.com/github_username/repo"><strong>Explore the docs »</strong></a>
     <br />
@@ -50,7 +52,7 @@
 
 Here's a blank template to get started:
 **To avoid retyping too much info. Do a search and replace with your text editor for the following:**
-`github_username`, `repo`, `twitter_handle`, `email`
+\`github_username\`, \`repo\`, \`twitter_handle\`, \`email\`
 
 
 ### Built With
@@ -70,20 +72,20 @@ To get a local copy up and running follow these simple steps.
 
 This is an example of how to list things you need to use the software and how to install them.
 * npm
-```sh
+\`\`\`sh
 npm install npm@latest -g
-```
+\`\`\`
 
 ### Installation
  
 1. Clone the repo
-```sh
+\`\`\`sh
 git clone https://github.com/github_username/repo.git
-```
+\`\`\`
 2. Install NPM packages
-```sh
+\`\`\`sh
 npm install
-```
+\`\`\`
 
 
 <!-- USAGE EXAMPLES -->
@@ -109,18 +111,21 @@ Contributions are what make the open source community such an amazing place to b
 
 
 name of contributer 
-dfsdfdfa
+${data.contribute}
 1. Fork the Project
-2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
-3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
-4. Push to the Branch (`git push origin feature/AmazingFeature`)
+2. Create your Feature Branch (\`git checkout -b feature/AmazingFeature\`)
+3. Commit your Changes (\`git commit -m 'Add some AmazingFeature'\`)
+4. Push to the Branch (\`git push origin feature/AmazingFeature\`)
 5. Open a Pull Request
 
 
 
 <!-- LICENSE -->
 ## License
-MIT
-Distributed under the MIT License. See `LICENSE` for more information.
+${data.license}
+Distributed under the MIT License. See \`LICENSE\` for more information.
 
-  
+  `;
+}
+
+module.exports = generateMarkdown;
