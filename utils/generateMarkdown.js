0ad0dd7159f0c1e381568f69bea19714e1b677d2
berhane <berhane4321@gmail.com>
2020-07-08 20:15:22 -0700

    modify  generatemarkdonw

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,13 +1,15 @@
-# <h1 style =" text-align:center;"> daily weather map</h1>
+// function to generate markdown for README
+function generateMarkdown(data) {
+  return `# <h1 style =" text-align:center;"> ${data.title}</h1>
 
 
 <!-- PROJECT LOGO -->
 <br />
 
-   <h1 style =" text-align:center;">daily weather map</h1>
+   <h1 style =" text-align:center;">${data.title}</h1>
 
     <p style =" text-align:center;">
-    world daily weather temp and humidiy of each city along with five day forcast
+    ${data.description}
    </p>
 
 
@@ -35,7 +37,7 @@
 
 Here's a blank template to get started:
 **To avoid retyping too much info. Do a search and replace with your text editor for the following:**
-`github_username`, `repo`, `twitter_handle`, `email`
+\`github_username\`, \`repo\`, \`twitter_handle\`, \`email\`
 
 
 <!-- GETTING STARTED -->
@@ -47,26 +49,26 @@ To get a local copy up and running follow these simple steps.
 
 This is an example of how to list things you need to use the software and how to install them.
 * npm
-```sh
+\`\`\`sh
 npm install npm@latest -g
-```
+\`\`\`
 
 ### Installation
  
 1. Clone the repo
-```sh
+\`\`\`sh
 git clone https://github.com/github_username/repo.git
-```
+\`\`\`
 2. Install NPM packages
-```sh
+\`\`\`sh
 npm install
-```
+\`\`\`
 
 
 <!-- USAGE EXAMPLES -->
 ## Usage
 
-to predicate temp and hubidity and show daily temp and humidity
+${data.usage}
 
 
 <!-- CONTRIBUTING -->
@@ -76,18 +78,21 @@ Contributions are what make the open source community such an amazing place to b
 
 
 name of contributer 
-berhane teklemariam
+${data.contribute}
 1. Fork the Project
-2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
-3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
-4. Push to the Branch (`git push origin feature/AmazingFeature`)
+2. Create your Feature Branch (\`git checkout -b feature/AmazingFeature\`)
+3. Commit your Changes (\`git commit -m 'Add some AmazingFeature'\`)
+4. Push to the Branch (\`git push origin feature/AmazingFeature\`)
 5. Open a Pull Request
 
 
 
 <!-- LICENSE -->
 ## License
-APACHE 2.0
-Distributed under the MIT License. See `LICENSE` for more information.
+${data.license}
+Distributed under the MIT License. See \`LICENSE\` for more information.
 
-  
+  `;
+}
+
+module.exports = generateMarkdown;
